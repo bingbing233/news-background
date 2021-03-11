cba1a8dd5dcc8dd504e46bf20684f8734691b8c3
liangbinghao <liangbinghao@codemao.cn>
2021-03-11 10:00:44 +0800

    数据库迁移文件修改为重构后的数据库

--- src/main/resources/migration/news_db.sql
+++ src/main/resources/migration/news_db.sql
@@ -1,30 +1,30 @@
-create table if not exists admin
+create table admin
 (
-    admin_id int default 0 not null
+    admin_id       int default 0 not null
         primary key,
-    admin_name varchar(50) null,
-    admin_contact varchar(50) null,
-    admin_password varchar(50) null
+    admin_name     varchar(50)   null,
+    admin_contact  varchar(50)   null,
+    admin_password varchar(50)   null
 );
 
-create table if not exists apartment
+create table apartment
 (
-    apartment_id int default 0 not null
+    apartment_id   int default 0 not null
         primary key,
-    apartment_name varchar(50) null
+    apartment_name varchar(50)   null
 );
 
-create table if not exists chars
+create table chars
 (
-    char_id int default 0 not null
+    char_id         int default 0 not null
         primary key,
-    char_name varchar(50) null,
-    char_permission varchar(50) null
+    char_name       varchar(50)   null,
+    char_permission varchar(50)   null
 );
 
-create table if not exists colume
+create table colume
 (
-    colume_id int auto_increment,
+    colume_id   int auto_increment,
     colume_name varchar(50) not null,
     constraint colume_colume_id_uindex
         unique (colume_id)
@@ -33,32 +33,31 @@ create table if not exists colume
 alter table colume
     add primary key (colume_id);
 
-create table if not exists employee
+create table employee
 (
-    emp_id int default 0 not null
+    emp_id       int default 0 not null
         primary key,
-    emp_login varchar(50) not null,
-    emp_name varchar(50) null,
-    emp_gender varchar(50) null,
-    emp_password varchar(50) not null,
-    contact varchar(150) null,
+    emp_login    varchar(50)   not null,
+    emp_name     varchar(50)   null,
+    emp_gender   varchar(50)   null,
+    emp_password varchar(50)   not null,
+    contact      varchar(150)  null,
     apartment_id int default 0 not null,
-    char_id int default 0 not null,
+    char_id      int default 0 not null,
     leave_or_not int default 0 not null
 );
 
-create table if not exists news
+create table news
 (
-    news_id int auto_increment,
-    news_title varchar(50) not null,
-    news_sum varchar(200) null,
-    news_source varchar(30) null,
-    public_time datetime null,
+    news_id      int auto_increment,
+    news_title   varchar(50)    not null,
+    news_sum     varchar(200)   null,
+    news_source  varchar(30)    null,
+    public_time  datetime       null,
     news_content varchar(10000) not null,
-    news_format_id int not null,
-    colume_id int null,
-    news_creator_id int not null,
-    public_state_id int not null,
+    `column`     varchar(20)    null,
+    news_creator varchar(20)    not null,
+    public_state varchar(20)    not null,
     constraint news_news_id_uindex
         unique (news_id)
 );
@@ -66,54 +65,54 @@ create table if not exists news
 alter table news
     add primary key (news_id);
 
-create table if not exists news_state
+create table news_state
 (
-    news_id int not null,
+    news_id    int not null,
     news_state int null
 );
 
-create table if not exists permission
+create table permission
 (
-    permission_id int default 0 not null
+    permission_id   int default 0 not null
         primary key,
-    permission_name varchar(50) null,
-    permission_des varchar(2000) null
+    permission_name varchar(50)   null,
+    permission_des  varchar(2000) null
 );
 
-create table if not exists permission_authority
+create table permission_authority
 (
     permission_id int not null
         primary key,
-    emp_id int null
+    emp_id        int null
 );
 
-create table if not exists pic
+create table pic
 (
-    pic_id int default 0 not null
+    pic_id   int default 0 not null
         primary key,
-    pic_des varchar(50) null,
-    news_id int default 0 not null,
-    pic_path varchar(50) null,
+    pic_des  varchar(50)   null,
+    news_id  int default 0 not null,
+    pic_path varchar(50)   null,
     pic_type int default 0 not null
 );
 
-create table if not exists public_state
+create table public_state
 (
-    public_state_id int default 0 not null
+    public_state_id   int default 0 not null
         primary key,
-    public_state_name varchar(50) null
+    public_state_name varchar(50)   null
 );
 
-create table if not exists review_record
+create table review_record
 (
-    review_id int default 0 not null
+    review_id     int default 0 not null
         primary key,
-    news_id int default 0 not null,
-    submiter_id int not null,
-    submiter_time datetime null,
-    reviewer_name varchar(50) null,
-    review_time datetime null,
-    review_remark varchar(800) null,
-    state_id int not null
+    news_id       int default 0 not null,
+    submiter_id   int           not null,
+    submiter_time datetime      null,
+    reviewer_name varchar(50)   null,
+    review_time   datetime      null,
+    review_remark varchar(800)  null,
+    state_id      int           not null
 );
 
